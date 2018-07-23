26876b88b0928e850a340e127665d4f215c80aa4
marion1998 <marion281098@outlook.fr>
2018-07-23 15:11:17 +0200

    creation base dossier

---
+++ test/footer.php
@@ -0,0 +1,3 @@
+<footer class="bg-dark text-light"> 
+<i class="far fa-copyright"></i> Beautiful footer
+</footer>
---
+++ test/header.php
@@ -0,0 +1,3 @@
+<header class="bg-dark text-light">
+<i class="fas fa-trophy"></i> Beautiful header
+</header>
