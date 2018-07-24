14ccd2dc67c8cf574072b69b2ed0f0a636129d2e
marion1998 <marion281098@outlook.fr>
2018-07-24 16:35:33 +0200

    modif exo5 et exo6 entier

--- loop/exo6.php
+++ loop/exo6.php
@@ -1,5 +1,5 @@
 <?php
-    $number = 1;
+    $number = 20;
 ?>
 
 <!DOCTYPE html>
@@ -8,22 +8,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Loop5</title>
+    <title>Loop6</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
 </head>
 <body>
     
-    <!-- En allant de 1 à 15 avec un pas de 1, afficher le message **On y arrive presque** -->
+    <!-- En allant de 20 à 0 avec un pas de 1, afficher le message **C'est presque bon**. -->
 
 <?php
-    for ($number = 1; $number <= 15; $number++) {
-        echo ($number . "." . " On y arrive presque" . "<br>");
+    for ($number = 20; $number >= 0; $number--) {
+        echo ($number . "." . "C'est presque bon" . "<br>");
     }
 ?>
 
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
 </body>
 </html>
